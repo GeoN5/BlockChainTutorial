f0b663ebd4c09949d77fac11ab2b1fca3ed51098
GeoN5 <sgepyh2916@naver.com>
2019-01-28 01:44:11 +0900

    simple prototype implement

--- src/util/Util.java
+++ src/util/Util.java
@@ -5,14 +5,15 @@ import java.security.MessageDigest;
 public class Util {
 
     public static String getHash(String input){
+
         StringBuilder result = new StringBuilder();
 
         try{
             MessageDigest md = MessageDigest.getInstance("SHA-256");
             md.update(input.getBytes());
             byte[] bytes = md.digest();
-            //문자열 변환
-            for (byte aByte : bytes) {
+
+            for (byte aByte : bytes) { //문자열 변환
                 result.append(Integer.toString((aByte & 0xff) + 0x100, 16).substring(1));
             }
         } catch (Exception e) {
